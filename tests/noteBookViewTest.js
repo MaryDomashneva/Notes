74fae297ab6e29bd972b2da552627a44de654005
marydomashneva <mary.domashneva@gmail.com>
2018-05-01 21:04:08 +0100

    HTML view notebook without using jquery

---
+++ tests/noteBookViewTest.js
@@ -0,0 +1,20 @@
+var NOTEBOOKVIEWMODULE = (function(exports) {
+
+  function testThatItReturnsRightString() {
+    var notebook = new NoteBook();
+    var note1 = new Note('AAAA');
+    var note2 = new Note('BBBB');
+    notebook.add(note1);
+    notebook.add(note2);
+    var view = new NoteBookView(notebook);
+    var actualString = view.HTMLNoteBookRepresentation();
+    var expectedString =
+     '<ul><li><div>AAAA</div></li><li><div>BBBB</div></li></ul>';
+    if (actualString !== expectedString) {
+      throw new Error (`Something went wrong! ${actualString} not equal\
+        ${expectedString}`);
+    }
+  };
+
+  testThatItReturnsRightString();
+})(this);
---
+++ views/noteBookView.js
@@ -0,0 +1,31 @@
+// $(document).ready(function() {
+//   notebook = new NoteBook();
+// });
+//
+// $(document).on('submit', 'form.NoteForm', function(event) {
+//   var text = document.getElementById('noteInputField').value;
+//   var note = new Note(text);
+//   notebook.add(note);
+//   event.preventDefault();
+//   for (var i = 0; i < notebook.notes.length; i++) {
+//     var note = document.createElement('h1');
+//     note.innerHTML = notebook.notes[i].text;
+//     $("body").append(note);
+//   };
+// });
+
+var NOTEBOOKVIEWMODULE = (function(exports) {
+  exports.NoteBookView = NoteBookView;
+  function NoteBookView(notebook) {
+    this.notebook = notebook;
+  };
+
+  NoteBookView.prototype.HTMLNoteBookRepresentation = function() {
+    var htmlString = ('<ul><li><div>' + (this.notebook.notes.map(note =>
+      note.abbreviation()).join('</div></li><li><div>')) + '</div></li></ul>');
+
+    return htmlString;
+  };
+
+  return exports;
+})(this);
